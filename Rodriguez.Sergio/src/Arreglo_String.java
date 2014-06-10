06ab5d29ac26a252d351b0d5be2545dfb3e37dab
Sergio <Sergio@10.20.1.203>
2014-06-09 19:23:40 -0500

    unidad2 lenguajes

--- Rodriguez.Sergio/src/Arreglo_String.java
+++ Rodriguez.Sergio/src/Arreglo_String.java
@@ -12,10 +12,16 @@ public class Arreglo_String {
 
     public static void main(String args[]){
     
-    //
+    //crear un arreglo que contenga las frutas que te gusten 
+        
+        String[] coctel={"sandia","manzana","mango","uvas","guayaba","durazno",};
     
-    
-    
+    //crear un cliclo for para iterar tus frutas
+        
+        for(String i:coctel){
+        System.out.println(i);
+        
+        }
     
     
     }
