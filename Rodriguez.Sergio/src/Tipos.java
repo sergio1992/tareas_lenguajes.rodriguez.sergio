a1f315e874181888c980e6610a557078343c4f48
Sergio <Sergio@10.20.1.203>
2014-06-09 19:14:50 -0500

    unidad2 lenguajes

---
+++ Rodriguez.Sergio/src/Tipos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sergio
+ */
+public class Tipos {
+    //static int algo;
+ public static void main(String args[]){
+ 
+ //Declaramos un arreglo de enteros: 
+     //int[] arreglo1 = new int [3];
+    // System.out.println(arreglo1[0]);
+   
+     //System.out.println(algo);
+     int[] arreglo1={-20,7,8,54};
+     //length=longitud es una propiedad de los arreglos 
+     for(int i=0;i<arreglo1.length;i++){
+         
+ System.out.println(arreglo1[i]);
+ System.out.println("Llenos");
+ 
+ for(int x:arreglo1){
+ System.out.println(x);
+ 
+ }
+ }
+ 
+ }   
+}
