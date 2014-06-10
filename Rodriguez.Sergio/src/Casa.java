d7ebdd483cc951437b36bb6511d8b4062e5ea2b8
Sergio <Sergio@10.20.1.203>
2014-06-09 19:49:57 -0500

    unidad2 lenguajes

---
+++ Rodriguez.Sergio/src/Casa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sergio
+ */
+public class Casa {
+    static int numeroAlbercas;
+    int numeroCuartos;
+    int niveles;
+
+    //suponer getters & setters
+    public int getNumeroCuartos() {
+        return numeroCuartos;
+    }
+
+    public void setNumeroCuartos(int numeroCuartos) {
+        this.numeroCuartos = numeroCuartos;
+    }
+
+    public int getNiveles() {
+        return niveles;
+    }
+
+    public void setNiveles(int niveles) {
+        this.niveles = niveles;
+    }
+    
+    
+    
+}
---
+++ Rodriguez.Sergio/src/UnidadResidencial.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sergio
+ */
+public class UnidadResidencial {
+    
+    public static void main(String args[]){
+    
+    Casa casa1=new Casa();
+    casa1.setNumeroCuartos(4);
+    casa1.setNiveles(5);
+    Casa casa2=new Casa();
+    casa2.setNiveles(3);
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+}
